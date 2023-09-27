00c7bd00c0991b35357fcb5b5cf78b7bf55446a7
jmukamani <j.mukamani@alustudent.com>
2023-09-27 15:08:10 +0200

    update file.staorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            new_dict = {}
+            for key, val in self.__objects.items():
+                if type(val) == cls:
+                    new_dict[key] = val
+            return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +55,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+        def delete(self, obj=None):
+            """delete obj from __objects if its inside"""
+            if obj is not None:
+                key = obj.to_dict()['__class__'] + '.' + obj.id
+                if key in self.__objects:
+                    del self.__objects[key]
+                    self.save()
