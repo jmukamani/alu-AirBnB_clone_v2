6de333e16ff83f1ee78bbc1750c96f0c584a8d8d
jmukamani <j.mukamani@alustudent.com>
2023-09-28 10:35:13 +0200

    update models and db storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable= False)
+
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+else:
+    @property
+    def cities(self):
+        """Getter for cities"""
+        cities_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return cities_list
