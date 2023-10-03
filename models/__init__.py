d421273891d54c06c44df22a5d805087ecfa95d6
jmukamani <j.mukamani>
2023-10-03 11:30:32 -0700

    console

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-import os import getenv
+from os import getenv
 
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
 
     storage = DBStorage()
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 
 """db_storage.py use database"""
-
+from sqlalchemy import create_engine, MetaData
 import os
-
-from models.base_model import Base
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-import sqlalchemy
-from sqlalchemy import scoped_session
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 class DBStorage:
@@ -47,13 +37,13 @@ class DBStorage:
 
     def all(self, cls=None):
         """Gets all objects depending on the class name"""
-        # from models.base_model import BaseModel
-        # from models.user import User
-        # from models.place import Place
-        # from models.state import State
-        # from models.city import City
-        # from models.amenity import Amenity
-        # from models.review import Review
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
         classes = {
             'User': User, 'Place': Place,
@@ -91,6 +81,14 @@ class DBStorage:
 
     def reload(self):
         """create all tables in the database"""
+        from models.base_model import Base
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
