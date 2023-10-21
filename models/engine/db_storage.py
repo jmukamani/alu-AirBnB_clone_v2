9c6596ccf734425d2bb459f3b585fae904f4d0ed
jmukamani <j.mukamani@alustudent.com>
2023-10-21 21:47:08 +0000

    update models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,8 +4,8 @@
 
 import os
 
+import sqlalchemy
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.amenity import Amenity
