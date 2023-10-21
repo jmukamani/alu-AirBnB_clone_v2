35f1f857c8aaf1bdd617dba76fc626fee4c4278d
jmukamani <j.mukamani@alustudent.com>
2023-10-21 21:28:25 +0000

    update models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,61 +16,86 @@ from models.user import User
 
 
 class DBStorage:
-    """ create tables in environmental"""
+    """Represents database storage"""
     __engine = None
     __session = None
 
+    """
+    To set the environment variables, depending on your operating system:
+    export MY_VARIABLE=<MY_VARIABLE_VALUE>  (Linux)
+    set MY_VARIABLE=<MY_VARIABLE_VALUE>  (Windows)
+    """
+
     def __init__(self):
-        """Initialize a new DBStorage instance."""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
-        if getenv("HBNB_ENV") == "test":
+        """Initialize object"""
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        database = os.getenv('HBNB_MYSQL_DB')
+
+        self.__engine = sqlalchemy.create_engine(
+            'mysql+mysqldb://{}:{}@{}:3306/{}'
+            .format(user,
+                    password,
+                    host,
+                    database), pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == "test":
+            # from models.base_model import Base
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """ """
-        dic = {}
-        if cls:
-            if type(cls) is str:
-                cls = eval(cls)
-            query = self.__session.query(cls)
-            for ele in query:
-                key = "{}.{}".format(type(ele).__name__, ele.id)
-                dic[key] = ele
-        else:
-            lists = [State, City, User, Place, Review, Amenity]
-            for classe in lists:
-                query = self.__session.query(classe)
-                for ele in query:
-                    key = "{}.{}".format(type(ele).__name__, ele.id)
-                    dic[key] = ele
+        """Gets all objects depending on the class name"""
+        # from models.base_model import BaseModel
+        # from models.user import User
+        # from models.place import Place
+        # from models.state import State
+        # from models.city import City
+        # from models.amenity import Amenity
+        # from models.review import Review
 
-        return (dic)
+        classes = {
+            'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+        obj_dict = {}
+        if cls is not None and cls in classes:
+            class_objects = self.__session.query(classes[cls]).all()
+            for obj in class_objects:
+                key = obj.__class__.__name__ + "." + obj.id
+                obj_dict[key] = obj
+
+        if cls is None:
+            for cls in classes:
+                class_objects = self.__session.query(classes[cls]).all()
+                for obj in class_objects:
+                    key = obj.__class__.__name__ + "." + obj.id
+                    obj_dict[key] = obj
+
+        return obj_dict
 
     def new(self, obj):
-        """Add obj to the current database session."""
+        """Adds the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes to the current database session."""
+        """Commits all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete obj from the current database session."""
-        if obj:
+        """Deletes from the current database session obj if not None"""
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and initialize a new session."""
+        """create all tables in the database"""
         Base.metadata.create_all(self.__engine)
-        ses = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(ses)
-        self.__session = Session()
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        session = scoped_session(session_factory)
+        self.__session = session()
 
     def close(self):
-        """Close the working SQLAlchemy session."""
+        """Close the session"""
         self.__session.close()
--- models/state.py
+++ models/state.py
@@ -10,21 +10,27 @@ from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
 
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
-
-else:
-    @property
-    def cities(self):
-        """Getter for cities"""
-        cities_list = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    else:
+        # if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        # @property
+        # def cities(self):
+        #     """ Returns the list of City instances with state_id
+        #     equals to the current State.id. """
+        #     all_cities = list(models.storage.all(City).values())
+        #     return list(filter(lambda city: (city.id == self.id),
+        #     all_cities))
+        # no time to fix
+        @property
+        def cities(self):
+            """ Returns the list of City instances with state_id
+            equals to the current State.id. """
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
